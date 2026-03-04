dce1bea89c83dd7c445bc2b849c4b6003c79a7aa
Youssef Ashraf <youseefashraf54@gmail.com>
2026-03-04 13:09:16 +0200

    Initial

---
+++ SortingVisual/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.example.sortingvisual {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.sortingvisual to javafx.fxml;
+    exports com.example.sortingvisual;
+}
